36078c7df99d8ec90a7c104575302bd13edeb736
Anoop Gupta <gupta.anoop.kumar@gmail.com>
2020-04-30 16:46:22 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,18 @@
 <script>
 	export let name;
 </script>
-
+<header>
+<h1>Hello {name}!</h1>
+</header>
 <main>
-	<h1>Hello {name}!</h1>
+	<h2>My First Svelte</h2>	
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
+	header {
+		text-align: center;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -19,6 +24,12 @@
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
+		font-weight: 100;
+	}
+	h2 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
 		font-weight: 100;
 	}
 
